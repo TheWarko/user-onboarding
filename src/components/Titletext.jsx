12370cbf1722528a07f6dcbd4d4152b24f6f96eb
Marco Savastio <marco.savastio@qmi.it>
2021-01-04 12:58:32 +0100

    Titletext component

---
+++ src/components/Titletext.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import stylesText from '../assets/styles/Text.module.scss'
+
+
+const Titletext = (props) => {
+    return (
+        <div className={props.className} >
+            <h1 className={stylesText.trex} >Contact details</h1>
+            <p>Welcome to United Properties, we are glad to see you! Let’s get started by letting us know a little bit about you</p>
+        </div>
+    )
+}
+
+export default Titletext
