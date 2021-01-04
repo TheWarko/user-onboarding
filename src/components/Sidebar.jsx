0a2a8ff009092dc81cbc51e9bba096a0142f0811
Marco Savastio <marco.savastio@qmi.it>
2021-01-04 12:57:56 +0100

    various small fixes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,8 +12,8 @@ const Sidebar = () => {
     return (
         <div className={stylesSidebar.sidebar} >
             <div className={stylesSidebar.container} >
-                <h1 className={`${stylesText.twocolors} ${stylesSpacing.mb200}`} >UNITED<span>PROPERTIES</span></h1>
-                <Checklist mb={stylesSpacing.mb64} />
+                <h1 className={`${stylesText.twocolors} ${stylesSpacing.mb192}`} >UNITED<span>PROPERTIES</span></h1>
+                <Checklist mb={stylesSpacing.mb80} />
                 <Quotes />
             </div>
         </div>
